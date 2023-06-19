6fc3660494e2008d804ef6be3ba30cc9295ffafe
Joanne0119 <114967562+Joanne0119@users.noreply.github.com>
2023-06-19 16:46:25 +0800

    Add files via upload

---
+++ script/RockPaperScissors.js
@@ -0,0 +1,110 @@
+let cScore = JSON.parse(localStorage.getItem('score')) || { //shortcut of if statement
+  meTot: 0,
+  comTot: 0,
+  tie: 0
+};
+console.log(cScore);
+updateScoreElem();
+// if(!cScore) //cScore === null (if cScore is not true)
+// {
+//   cScore = {
+//     meTot: 0,
+//     comTot: 0,
+//     tie: 0
+//   }
+// }
+
+function comp(rpc)
+{
+  let com = Math.floor(Math.random() * 3);
+  let comMove = '', yourMove = '', result = '';
+  console.log(com);
+  switch(rpc)
+  {
+    case 0 :
+      yourMove = '✊🏻';
+      if(com === 1) 
+      {
+        comMove = '🖐🏻'; 
+        result = 'You Lose !';
+      }
+      else if(com === 0)
+      {
+        comMove = '✊🏻'; 
+        result= 'Tie !';
+      }
+      else 
+      {
+        comMove = '✌🏻';
+        result = 'You Win !';
+      }
+      break;
+    case 1 :
+      yourMove = '🖐🏻';
+      if(com === 2) 
+      {
+        comMove = '✌🏻';
+        result =  'You Lose !';
+      }
+      else if(com === 1)
+      {
+        comMove = '🖐🏻';
+        result =  'Tie !';
+      }
+      else 
+      {
+        comMove = '✊🏻';
+        result =  'You Win !';
+      }
+      break;
+    case 2 :
+      yourMove = '✌🏻';
+      if(com === 0) 
+      {
+        comMove = '✊🏻';
+        result =  'You Lose !';
+      }
+      else if(com === 2)
+      {
+        comMove = '✌🏻';
+        result =  'Tie !';
+      }
+      else 
+      {
+        comMove = '🖐🏻';
+        result = 'You Win !';
+      }
+      break;
+  }
+  fScore(result);
+  // alert(`Computer is ${comMove}, ${result}\nYou : ${cScore.meTot}, Computer : ${cScore.comTot}, Tie : ${cScore.tie}`);
+  document.querySelector('.js-result')
+    .innerHTML = result;
+  document.querySelector('.js-play')
+    .innerHTML = `You <span class="yourmove">${yourMove}</span>  <span class="commove">${comMove}</span>Computer`;
+}
+
+function updateScoreElem()
+{
+  document.querySelector('.js-score')
+    .innerHTML = `You : ${cScore.meTot}, Computer : ${cScore.comTot}, Tie : ${cScore.tie}`;
+}
+
+function fScore(result)
+{
+  if(result === 'You Win !') cScore.meTot++;
+  else if(result === 'You Lose !') cScore.comTot ++;
+  else cScore.tie++;
+  localStorage.setItem('score', JSON.stringify(cScore)); 
+//using the localStorage to save the cScore, this can make the score won't back to 0 when we refresh 
+  updateScoreElem();
+}
+
+function restart()
+{
+  cScore.meTot = 0;
+  cScore.comTot = 0;
+  cScore.tie = 0;
+  localStorage.removeItem('score'); //removeItem will turn the value into null, so we need to write if statement to give it default value
+  updateScoreElem();
+}
